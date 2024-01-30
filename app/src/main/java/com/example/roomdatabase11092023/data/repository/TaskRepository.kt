b3ae8ff94553f93cbe82a4629a116ca05659a5a5
phamtanphat <phatdroid94@gmail.com>
2024-01-30 20:23:36 +0700

    repository

---
+++ app/src/main/java/com/example/roomdatabase11092023/data/repository/TaskRepository.kt
@@ -0,0 +1,21 @@
+package com.example.roomdatabase11092023.data.repository
+
+import android.content.Context
+import com.example.roomdatabase11092023.data.database.AppDAO
+import com.example.roomdatabase11092023.data.database.AppDatabase
+import com.example.roomdatabase11092023.data.database.enity.Task
+
+class TaskRepository (private var context: Context) {
+    private val appDao: AppDAO by lazy {
+        AppDatabase.getInStance(context).appDao()
+    }
+
+    fun getListTask(): List<Task> {
+        return appDao.getListTask()
+    }
+
+    fun addTask(task: Task): Long {
+        return appDao.addTask(task)
+    }
+
+}
